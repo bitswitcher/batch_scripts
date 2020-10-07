d1306cd2ad3043778641ff3ae4bf10ec46821f61
kotani <koichi.1.tani@jp.onkyo.com>
2020-10-07 13:38:34 +0900

    delete chcp and cd alias to avoid abnormal behavior of batch script

--- Cmd_profile.bat
+++ Cmd_profile.bat
@@ -2,7 +2,7 @@
 
 rem change code page
 rem  932(shift_jis) / 51932 (euc-jp) / 65001(utf-8)
-chcp 932
+rem chcp 932
 
 rem alias
 doskey pwd=cd
@@ -21,26 +21,6 @@ doskey less=more /e $*
 doskey lv=more /e $*
 doskey ifc=ipconfig
 doskey pingl=ping 192.168.$1
-
-rem alias (cd)
-set PREV_PWD_FILE=%TEMP%\pwd.prev
-set PREV_PWD_TEMP=%TEMP%\pwd.temp
-doskey cd=if ""=="$1" (^
-    @cd ^
-) else if "-"=="$1" (^
-    @for /f "usebackq tokens=*" %%i in ^(`cd`^) do @echo %%i^>"%PREV_PWD_TEMP%" ^& ^
-    @for /f "usebackq tokens=*" %%i in ^("%PREV_PWD_FILE%"^) do @cd "%%i" ^&^& ^
-    @type "%PREV_PWD_FILE%" ^& ^
-    @type "%PREV_PWD_TEMP%" ^> "%PREV_PWD_FILE%" ^
-) else (^
-    @for /f "usebackq tokens=*" %%i in ^(`cd`^) do @echo %%i^>"%PREV_PWD_TEMP%" ^& @cd $* ^&^& @type "%PREV_PWD_TEMP%" ^> "%PREV_PWD_FILE%" ^
-)
-set PREV_PWD_FILE=
-set PREV_PWD_TEMP=
-
-rem clear initial display
-cls
-echo %cd%
 
 rem prompt setting
 prompt $$$s
